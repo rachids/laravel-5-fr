5723ef3b7125d2acd74ac65f4710edd9834c142e
David Zaouch <david.zaouch@chokkanweb.fr>
2015-02-26 15:46:17 +0100

    Fichiers concernant les mots de passe

--- resources/lang/fr/passwords.php
+++ resources/lang/fr/passwords.php
@@ -1,22 +1,9 @@
 <?php
 
 return [
-
-	/*
-	|--------------------------------------------------------------------------
-	| Password Reminder Language Lines
-	|--------------------------------------------------------------------------
-	|
-	| The following language lines are the default lines which match reasons
-	| that are given by the password broker for a password update attempt
-	| has failed, such as for an invalid token or invalid new password.
-	|
-	*/
-
-	"password" => "Passwords must be at least six characters and match the confirmation.",
-	"user" => "We can't find a user with that e-mail address.",
-	"token" => "This password reset token is invalid.",
-	"sent" => "We have e-mailed your password reset link!",
-	"reset" => "Your password has been reset!",
-
+  "password" => "Les mots de passe doivent avoir au moins six caractères et doivent être identiques.",
+  "user" => "Nous ne pouvons trouver cet utilisateur avec cette adresse e-mail.",
+  "token" => "Ce jeton de réinitialisation du mot de passe n'est pas valide.",
+  "sent" => "Nous vous avons envoyé un email de réinitialisation de mot de passe !",
+  "reset" => "Votre mot de passe a été réinitialisé",
 ];
